6329c276da85c93a6f0798c02ae934b686e8d4fd
CHRIS GEORGE <Chrisgeorge2k20@gmail.com>
2024-08-26 23:26:54 +0530

    update

--- client/src/pages/Article.js
+++ client/src/pages/Article.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import articleContent from "./article-content";
 
 const Article = () => {
   const { name } = useParams();
+  const article = articleContent.find((article) => article.name === name);
+  if (!article) return <h1>Article does not exits</h1>;
   return (
     <div>
       <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">
-        This is the {name} Article
+        {article.title}
       </h1>
+      {article.content.map((paragraph, index) => (
+        <p className="mx-auto leading-relaxed text-base mb-4 key={index}">
+          {paragraph}
+        </p>
+      ))}
     </div>
   );
 };
